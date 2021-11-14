1c5742297bb94a7a444cf08a37a723e46d081923
Radovan Sroka <rsroka@redhat.com>
2021-11-14 18:01:06 +0100

    url encoded

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,15 @@
-//const express = require('express');
 const { check, validationResult } = require('express-validator');
-
 const Router = require('express-promise-router')
 
 const path = require('path');
-const auth = require('http-auth');
 
 const db = require('../db/pg')
 
 
 
 const router =  new Router();
+
+
 
 router.all('/', (req, res) => {
   res.redirect('main');
@@ -43,6 +42,89 @@ router.post('/login',
                 });
               }
 });
+
+
+router.get('/query', async (req, res) => {
+
+  var sql = 'SELECT * FROM my_view '
+  var where = " WHERE "
+  var query = []
+
+  const qs = new URLSearchParams(req.query)
+
+
+  // const car_plate = req.body.car_plate;
+
+  if (qs.has('car_plate')) {
+    query.push(` car_plate = '${qs.get('car_plate')}' `)
+    req.body.car_plate = qs.get('car_plate')
+  }
+
+  // const car_model = req.body.car_model;
+
+  if (qs.has('car_model')) {
+    var model = qs.get('car_model').split(" ").join('.*')
+    query.push(` lower(car_model) ~* '.*${model.toLowerCase()}.*' `)
+    req.body.car_model = qs.get('car_model')
+  }
+
+  // const type_name = req.body.type_name;
+
+  if (qs.has('type_name')) {
+    query.push(` lower(type_name) ~* '.*${qs.get('type_name').toLowerCase()}.*' `)
+    req.body.type_name = qs.get('type_name')
+  }
+
+
+  // const car_color = req.body.car_color;
+
+  if (qs.has('car_color')) {
+    query.push(` lower(car_color) ~* '.*${qs.get('car_color').toLowerCase()}.*' `)
+    req.body.car_color = qs.get('car_color')
+  }
+
+
+  //const car_year_from = req.body.car_year_from;
+
+  if (qs.has('car_year_from')) {
+    query.push(`car_year >= '${qs.get('car_year_from')}'`)
+    req.body.car_year_from = qs.get('car_year_from')
+  }
+
+  // const car_year_to = req.body.car_year_to;
+
+  if (qs.has('car_year_to')) {
+    query.push(`car_year <= '${qs.get('car_year_to')}'`)
+    req.body.car_year_to = qs.get('car_year_to')
+  }
+
+  if (!(query.length === 0)) {
+    sql = sql + where + query.join(' AND ')
+  }
+
+  var limit = '10000'
+
+  if (qs.has('limit')) {
+    limit = qs.get('limit')
+  }
+
+  sql = sql + ' ORDER BY car_year LIMIT ' + limit + ';';
+
+  console.log(sql)
+
+  const result = await db.query({
+    rowMode: 'array',
+    text: sql,
+  });
+
+  res.render('main', {
+    title: 'Query Table',
+    data: req.body,
+    rows: result.rows,
+  });
+
+});
+
 
 router.get('/main', async (req, res) => {
 
@@ -79,76 +161,61 @@ router.post('/main',
 
   if (!errors.isEmpty()) {
     res.render('main', {
-      title: 'Query Table5',
+      title: 'Query Table',
       errors: errors.array(),
       data: req.body,
       rows: [],
     });
   }
 
-  var sql = 'SELECT * FROM my_view '
-  var where = " WHERE "
-  var query = []
+  var qs = []
 
   const car_plate = req.body.car_plate;
 
   if (! (car_plate === '')) {
-    query.push(` car_plate = '${car_plate}' `)
+    qs.push(`car_plate=${car_plate}`)
   }
 
   const car_model = req.body.car_model;
 
   if (! (car_model === '')) {
     var model = car_model.split(" ").join('.*')
-    query.push(` lower(car_model) ~* '.*${model.toLowerCase()}.*' `)
+    qs.push(`car_model=${car_model}`)
   }
 
 
   const type_name = req.body.type_name;
 
   if (! (type_name === '')) {
-    query.push(` lower(type_name) ~* '.*${type_name.toLowerCase()}.*' `)
+    qs.push(`type_name=${type_name}`)
   }
 
 
   const car_color = req.body.car_color;
 
   if (! (car_color === '')) {
-    query.push(` lower(car_color) ~* '.*${car_color.toLowerCase()}.*' `)
+    qs.push(`car_color=${car_color}`)
   }
 
 
   const car_year_from = req.body.car_year_from;
 
   if (! (car_year_from === '')) {
-    query.push(`car_year >= '${car_year_from}'`)
+    qs.push(`car_year_from=${car_year_from}`)
   }
 
   const car_year_to = req.body.car_year_to;
 
   if (! (car_year_to === '')) {
-    query.push(`car_year <= '${car_year_to}'`)
+    qs.push(`car_year_to=${car_year_to}`)
   }
 
-
-  if (!(query.length === 0)) {
-    sql = sql + where + query.join(' AND ')
+  if (!(qs.length === 0)) {
+    qs = 'query?' + encodeURIComponent(qs.join('&'))
+  } else {
+    qs = 'query'
   }
-
-  sql = sql + ' ORDER BY car_year LIMIT 10000; ';
-  console.log(sql)
-
-  const result = await db.query({
-    rowMode: 'array',
-    text: sql,
-  });
-
-  res.render('main', {
-    title: 'Query Table',
-    data: req.body,
-    rows: result.rows,
-  });
-
+  res.redirect(qs)
 });
 
 module.exports = router;
